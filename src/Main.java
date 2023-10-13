35dd3855deef7f9dbbd4fe974fe1266bb2972c68
ravitejabl <i.ravitejabl@gmail.com>
2023-10-14 01:22:32 +0530

    linkedlist

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+      LL list =  new LL();
+      list.insertFirst(3);
+        list.insertFirst(4);
+        list.insertFirst(5);
+        list.insertFirst(9);
+        list.display();
+
+    }
+}
